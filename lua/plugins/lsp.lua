0e8421d779fe33be58c992e5cc28d2a5d25a75c5
hisuic <henryharlay@gmail.com>
2026-05-28 02:46:46 +0900

    fix: lsp settings: add basic settings

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,5 +1,75 @@
 return {
   {
-    "neovim/nvim-lspconfig",
-  }
+    "mason-org/mason.nvim",
+    cmd = "Mason",
+    config = function()
+      require("mason").setup()
+    end,
+  },
+
+  {
+    "mason-org/mason-lspconfig.nvim",
+    dependencies = {
+      "mason-org/mason.nvim",
+      "neovim/nvim-lspconfig",
+    },
+    event = { "BufReadPre", "BufNewFile" },
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = {
+          "lua_ls",
+          "ts_ls",
+          "html",
+          "cssls",
+
+          -- added
+          "pyright",
+          "clangd",
+          "rust_analyzer",
+          "bashls",
+          "jsonls",
+          "yamlls",
+          "marksman",
+        },
+      })
+
+      vim.lsp.config("lua_ls", {
+        settings = {
+          Lua = {
+            diagnostics = {
+              globals = { "vim" },
+            },
+          },
+        },
+      })
+
+      vim.lsp.config("ts_ls", {})
+      vim.lsp.config("html", {})
+      vim.lsp.config("cssls", {})
+
+      vim.lsp.config("pyright", {})
+      vim.lsp.config("clangd", {})
+      vim.lsp.config("rust_analyzer", {})
+      vim.lsp.config("bashls", {})
+      vim.lsp.config("jsonls", {})
+      vim.lsp.config("yamlls", {})
+      vim.lsp.config("marksman", {})
+
+      vim.lsp.enable({
+        "lua_ls",
+        "ts_ls",
+        "html",
+        "cssls",
+
+        -- added
+        "pyright",
+        "clangd",
+        "rust_analyzer",
+        "bashls",
+        "jsonls",
+        "yamlls",
+        "marksman",
+      })
+    end,
+  },
 }
