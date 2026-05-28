2426895662245052bf211a22e492555ee0724156
hisuic <henryharlay@gmail.com>
2026-05-28 09:51:38 +0900

    add markview

---
+++ lua/plugins/markview.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    "OXY2DEV/markview.nvim",
+    lazy = false,
+
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-tree/nvim-web-devicons",
+    },
+
+    config = function()
+      require("markview").setup({
+        preview = {
+          icon_provider = "devicons",
+        },
+      })
+    end,
+  },
+}
